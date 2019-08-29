d462f5d7d34f573fecb459dd7bd77645e398f9b5
SaiChandra2896 <saichandra2896@gmail.com>
2019-08-29 17:15:44 +0530

    add rough buttons to check navigation

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CategoriesMealsScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>the CategoriesMealsScreen </Text>
+      <Button
+        title='go to mealdetail'
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'MealDetail' });
+        }}
+      />
+      <Button
+        title='Back'
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
     </View>
   );
 };
